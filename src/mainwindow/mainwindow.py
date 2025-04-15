75fe578cde4759bc9f7bb771edab97cbc6af5d9d
Damian Dudycz <damiandudycz@yahoo.com>
2025-04-15 21:51:47 +0200

    Move window to subdir

---
+++ src/mainwindow/mainwindow.py
@@ -0,0 +1,22 @@
+from gi.repository import Adw
+from gi.repository import Gtk
+from gi.repository import Gio
+
+@Gtk.Template(resource_path='/com/damiandudycz/CatalystLab/mainwindow/mainwindow.ui')
+class CatalystlabWindow(Adw.ApplicationWindow):
+    __gtype_name__ = 'CatalystlabWindow'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Define the window action
+        action_toggle_sidebar = Gio.SimpleAction.new("toggle-sidebar", None)
+        action_toggle_sidebar.connect("activate", self.on_toggle_sidebar)
+        self.add_action(action_toggle_sidebar)
+
+    split_view = Gtk.Template.Child()
+
+    def on_toggle_sidebar(self, action, parameter):
+        # Toggle the collapsed state of the split view
+        self.split_view.set_show_sidebar(not self.split_view.get_show_sidebar())
+
