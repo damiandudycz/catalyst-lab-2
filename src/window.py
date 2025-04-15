3784d73fe79130e50397671ff36989d30fc491b1
Damian Dudycz <damiandudycz@yahoo.com>
2025-04-15 16:09:56 +0200

    Convert code to use AdwOverlaySplitView

--- src/window.py
+++ src/window.py
@@ -19,6 +19,7 @@
 
 from gi.repository import Adw
 from gi.repository import Gtk
+from gi.repository import Gio
 
 @Gtk.Template(resource_path='/com/damiandudycz/CatalystLab/window.ui')
 class CatalystlabWindow(Adw.ApplicationWindow):
@@ -26,3 +27,15 @@ class CatalystlabWindow(Adw.ApplicationWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # Define the window action
+        action_toggle_sidebar = Gio.SimpleAction.new("toggle-sidebar", None)
+        action_toggle_sidebar.connect("activate", self.on_toggle_sidebar)
+        self.add_action(action_toggle_sidebar)
+
+    split_view = Gtk.Template.Child()
+
+    def on_toggle_sidebar(self, action, parameter):
+        # Toggle the collapsed state of the split view
+        self.split_view.set_show_sidebar(not self.split_view.get_show_sidebar())
+
