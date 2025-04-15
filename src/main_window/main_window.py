b4a474d6ef66e36e5715bab3bedaa0b5e329ac03
Damian Dudycz <damiandudycz@yahoo.com>
2025-04-15 22:10:01 +0200

    Reduce not needed parts of code

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -1,22 +1,16 @@
 from gi.repository import Adw
 from gi.repository import Gtk
-from gi.repository import Gio
 
 @Gtk.Template(resource_path='/com/damiandudycz/CatalystLab/main_window/main_window.ui')
 class CatalystlabWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'CatalystlabWindow'
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        # Define the window action
-        action_toggle_sidebar = Gio.SimpleAction.new("toggle-sidebar", None)
-        action_toggle_sidebar.connect("activate", self.on_toggle_sidebar)
-        self.add_action(action_toggle_sidebar)
-
+    # Main view - overlay split view.
     split_view = Gtk.Template.Child()
 
-    def on_toggle_sidebar(self, action, parameter):
-        # Toggle the collapsed state of the split view
+    # Toggle sidebar visiblity with button.
+    @Gtk.Template.Callback()
+    def sidebar_toggle_button_clicked(self, button):
+        """Callback function that is called when we click the button"""
         self.split_view.set_show_sidebar(not self.split_view.get_show_sidebar())
 
