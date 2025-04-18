9f52b9f4b04a0433fca76771425eba4885c5593c
Damian Dudycz <damiandudycz@yahoo.com>
2025-04-18 22:33:54 +0200

    Fixed side menu hiding/showing issues

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -13,6 +13,8 @@ class CatalystlabWindow(Adw.ApplicationWindow):
     split_view = Gtk.Template.Child()
     side_menu = Gtk.Template.Child()
     content_view = Gtk.Template.Child()
+    sidebar_toggle_breakpoint = Gtk.Template.Child()
+    sidebar_toggle_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -22,12 +24,16 @@ class CatalystlabWindow(Adw.ApplicationWindow):
         EventBus.subscribe(AppEvents.OPEN_ABOUT, self.open_about_section)
         # Load initial section page.
         self.content_view.load_main_section(self.side_menu.selected_section)
+        # Connect sidebar_toggle_breakpoint actions
+        self.sidebar_toggle_breakpoint.connect("apply", self._on_sidebar_toggle_breakpoint_apply)
+        self.sidebar_toggle_breakpoint.connect("unapply", self._on_sidebar_toggle_breakpoint_unapply)
 
     # Toggle sidebar visibility with button.
     @Gtk.Template.Callback()
     def sidebar_toggle_button_clicked(self, _):
         """Callback function that is called when we click the button"""
-        self.split_view.set_show_sidebar(not self.split_view.get_show_sidebar())
+        if self.allow_side_menu:
+            self.split_view.set_show_sidebar(not self.split_view.get_show_sidebar())
 
     # Bind displayed content to side menu selected page.
     @Gtk.Template.Callback()
@@ -35,9 +41,26 @@ class CatalystlabWindow(Adw.ApplicationWindow):
         self.content_view.load_main_section(section)
 
     def open_about_section(self, *args, **kwargs):
-        print("Open Abot")
         self.content_view.load_main_section(AppSection.PROJECTS)
 
+    # Managing side bar and toggle side bar button visibility for selected section and collapsed state:
+
     def set_sidebar_visible(self, visible: bool):
-        self.split_view.set_show_sidebar(visible)
+        self.allow_side_menu = visible
+        if (visible and self.allow_side_menu) or not visible:
+            self.split_view.set_show_sidebar(visible and not self.split_view.get_collapsed())
+            self.sidebar_toggle_button.set_visible(self.allow_side_menu and self.split_view.get_collapsed())
+
+    def _on_sidebar_toggle_breakpoint_apply(self, breakpoint):
+        self.split_view.set_collapsed(True)
+        self.split_view.set_show_sidebar(False)
+        self.sidebar_toggle_button.set_visible(self.allow_side_menu)
+
+    def _on_sidebar_toggle_breakpoint_unapply(self, breakpoint):
+        self.split_view.set_collapsed(False)
+        self.sidebar_toggle_button.set_visible(False)
+        if self.allow_side_menu:
+            self.split_view.set_show_sidebar(True)
+        else:
+            self.split_view.set_show_sidebar(False)
 
