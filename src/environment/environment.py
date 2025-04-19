57cb5d5bd62a7d1ab3f864fbc2e2e5924a5883a3
Damian Dudycz <damiandudycz@yahoo.com>
2025-04-20 00:30:35 +0200

    Cleanup

---
+++ src/environment/environment.py
@@ -0,0 +1,6 @@
+# Manage external tools used by this project, like catalyst, releng, qemu etc.
+# It can get the tools from host system or flatpak virtual environment.
+
+class EnvTools:
+    pass
+
--- src/helper_types/gtkboxabcmeta.py
+++
@@ -1,6 +0,0 @@
-from abc import ABCMeta, abstractmethod
-from gi.repository import Gtk
-
-# Abstract class that must inherit Gtk.Box
-class GtkBoxABCMeta(type(Gtk.Box), ABCMeta):
-    pass
