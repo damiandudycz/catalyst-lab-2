bda9da1cd6a390b6e7376534705e668232d1851c
Damian Dudycz <damiandudycz@yahoo.com>
2025-05-27 21:40:42 +0200

    Empty section for Releng

--- src/ui/app_sections/releng/releng_section.py
+++ src/ui/app_sections/releng/releng_section.py
@@ -2,8 +2,11 @@ from gi.repository import Gtk
 from gi.repository import Adw
 from .app_section import app_section
 
-@app_section(title="Releng")
+@app_section(title="Releng", icon="book-minimalistic-svgrepo-com-symbolic", order=4_000)
+@Gtk.Template(resource_path='/com/damiandudycz/CatalystLab/ui/app_sections/releng/releng_section.ui')
 class RelengSection(Gtk.Box):
+    __gtype_name__ = "RelengSection"
+
     def __init__(self, content_navigation_view: Adw.NavigationView, **kwargs):
         super().__init__(**kwargs)
 
