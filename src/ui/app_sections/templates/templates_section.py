371fbde84faf54621e8cd29bfa0aa841348b74a1
damiandudycz <damiandudycz@yahoo.com>
2025-07-29 17:29:31 +0200

    Clean unused sections

--- src/ui/app_sections/templates/templates_section.py
+++ src/ui/app_sections/templates/templates_section.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk
 from gi.repository import Adw
 from .app_section import app_section
 
-@app_section(title="Templates")
+#@app_section(title="Templates")
 class TemplatesSection(Gtk.Box):
     def __init__(self, content_navigation_view: Adw.NavigationView, **kwargs):
         super().__init__(**kwargs)
